9350bfa11f2c8d59a7688fd84043c31b232cedf0
seraj.alam <seraj.alam@wisdmlabs.com>
2023-01-17 13:03:55 +0530

    session 2

--- src/App.js
+++ src/App.js
@@ -4,13 +4,30 @@ class App extends React.Component {
 	// constructor method begins here:
 constructor(props) {
   super(props);
-  this.state = { title: 'Best App' };
+  this.state = { title: 'Best App', isChanged : true, secondTitle: "New App" };
+
+  this.updateS = this.updateState.bind(this);
 }
-	
+	updateState(){
+    
+    this.setState({ isChanged:false});
+
+  }
+
+  componentDidMount(){
+    let self=this;
+    setTimeout(function() {
+      self.setState({isChanged:false}); 
+    }, 3000);
+    
+  }
+
+
   render() {
     return (
       <h1>
-        {this.state.title}
+        <button onClick={this.updateS}>Change</button>
+        {this.state.isChanged ? this.state.title : this.state.secondTitle}
       </h1>
     );
   }
